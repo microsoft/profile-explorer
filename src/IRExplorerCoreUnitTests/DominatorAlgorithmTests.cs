ea532bcb6c8a09d3b68f63b0d60f42c0fa379aca
Andrew Dean <adean@microsoft.com>
2020-07-28 13:44:46 -0700

    Refactor

--- src/IRExplorerCoreUnitTests/DominatorAlgorithmTests.cs
+++ src/IRExplorerCoreUnitTests/DominatorAlgorithmTests.cs
@@ -11,34 +11,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace IRExplorerCoreTests {
     [TestClass]
     public class DominatorAlgorithmTests {
-        string functionText =
-@"BLOCK 0 Out(1)                                            
-ENTRY        ___local_stdio_printf_options ()                           #90
-BLOCK 1 In(0) Out(3)                                      
-PRAGMA          OPBLKSTART Level: 2                                     #90
-                OPRET     &?_OptionsStorage@?1??__local_stdio_printf_options@@9@9 (4|N=4) #92
-                OPGOTO    &$LN1                           (?|N=4)       #92
-BLOCK 2 Out(3)                                            
-PRAGMA          OPBLKEND Level: 2                                       #93
-BLOCK 3 In(1,2) Out(4)                                    
-$LN1@local_stdi:                                          ; uses = 1 
-BLOCK 4 In(3)                                             
-EXIT                                                                    #93
-BLOCK                                                     ";
-
-        string fixedFunctionText =
-@"BLOCK 0 Out(1)
-ENTRY        func ()
-BLOCK 1 In(0) Out(2,3)
-BLOCK 2 In(1) Out(3)
-BLOCK 3 In(1,2) Out(4)
-BLOCK 4 In(3)
-EXIT
-BLOCK";
-
         [TestMethod]
         public void FallbackAlgorithmTerminatesForDominators() {
-            var function = new UTCParser(functionText, null, null).Parse();
+            var function = TestFunctions.QuirkyFunction;
             var dominatorAlgorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.Dominators);
             Assert.IsTrue(dominatorAlgorithm.Dominates(function.BlockByNumber(1), function.BlockByNumber(3)));
             Assert.IsFalse(dominatorAlgorithm.Dominates(function.BlockByNumber(3), function.BlockByNumber(1)));
@@ -46,7 +21,7 @@ BLOCK";
 
         [TestMethod]
         public void QueryCacheAndFallbackAlgorithmMatchForDominatorsWithNormalIR() {
-            var function = new UTCParser(fixedFunctionText, null, null).Parse();
+            var function = TestFunctions.SimpleFunction;
             var fallbackAlgorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.Dominators);
             var cachedAlgorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.Dominators | DominatorAlgorithmOptions.BuildQueryCache);
             for (ushort i = 0; i < 5; ++i) {
@@ -60,7 +35,7 @@ BLOCK";
 
         [TestMethod]
         public void QueryCacheAndFallbackAlgorithmMatchForDominatorsWithQuirkyIR() {
-            var function = new UTCParser(functionText, null, null).Parse();
+            var function = TestFunctions.QuirkyFunction;
             var fallbackAlgorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.Dominators);
             var cachedAlgorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.Dominators | DominatorAlgorithmOptions.BuildQueryCache);
             for (ushort i = 0; i < 5; ++i) {
@@ -74,7 +49,7 @@ BLOCK";
 
         [TestMethod]
         public void FallbackAlgorithmTerminatesForPostDominators() {
-            var function = new UTCParser(functionText, null, null).Parse();
+            var function = TestFunctions.QuirkyFunction;
             var dominatorAlgorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.PostDominators);
             Assert.IsFalse(dominatorAlgorithm.Dominates(function.BlockByNumber(1), function.BlockByNumber(3)));
             Assert.IsTrue(dominatorAlgorithm.Dominates(function.BlockByNumber(3), function.BlockByNumber(1)));
@@ -83,7 +58,7 @@ BLOCK";
 
         [TestMethod]
         public void QueryCacheAndFallbackAlgorithmMatchForPostDominatorsWithNormalIR() {
-            var function = new UTCParser(fixedFunctionText, null, null).Parse();
+            var function = TestFunctions.SimpleFunction;
             var fallbackAlgorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.PostDominators);
             var cachedAlgorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.PostDominators | DominatorAlgorithmOptions.BuildQueryCache);
             for (ushort i = 0; i < 5; ++i) {
@@ -97,7 +72,7 @@ BLOCK";
 
         [TestMethod]
         public void QueryCacheAndFallbackAlgorithmMatchForPostDominatorsWithQuirkyIR() {
-            var function = new UTCParser(functionText, null, null).Parse();
+            var function = TestFunctions.QuirkyFunction;
             var fallbackAlgorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.PostDominators);
             var cachedAlgorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.PostDominators | DominatorAlgorithmOptions.BuildQueryCache);
             for (ushort i = 0; i < 5; ++i) {
@@ -111,7 +86,7 @@ BLOCK";
 
         [TestMethod]
         public void GetDominatorsReturnsAllNodesThatDominateProvidedNodeForNormalIR() {
-            var function = new UTCParser(fixedFunctionText, null, null).Parse();
+            var function = TestFunctions.SimpleFunction;
             var algorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.Dominators | DominatorAlgorithmOptions.BuildQueryCache);
 
             // entry node has no dominators
@@ -142,7 +117,7 @@ BLOCK";
 
         [TestMethod]
         public void GetDominatorsReturnsAllNodesThatDominateProvidedNodeForQuirkyIR() {
-            var function = new UTCParser(functionText, null, null).Parse();
+            var function = TestFunctions.QuirkyFunction;
             var algorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.Dominators | DominatorAlgorithmOptions.BuildQueryCache);
 
             // block 0 has no dominators
@@ -173,7 +148,7 @@ BLOCK";
 
         [TestMethod]
         public void GetPostDominatorsReturnsAllNodesThatPostDominateProvidedNodeForNormalIR() {
-            var function = new UTCParser(fixedFunctionText, null, null).Parse();
+            var function = TestFunctions.SimpleFunction;
             var algorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.PostDominators | DominatorAlgorithmOptions.BuildQueryCache);
 
             // block 0 is post-dominated by blocks 1, 3, and 4
@@ -204,7 +179,7 @@ BLOCK";
 
         [TestMethod]
         public void GetPostDominatorsReturnsAllNodesThatPostDominateProvidedNodeForQuirkyIR() {
-            var function = new UTCParser(functionText, null, null).Parse();
+            var function = TestFunctions.QuirkyFunction;
             var algorithm = new DominatorAlgorithm(function, DominatorAlgorithmOptions.PostDominators | DominatorAlgorithmOptions.BuildQueryCache);
 
             // block 0 is post-dominated by blocks 1, 3, and 4
