1acfaed640b8205908727fd15be7cdc232ca1834
Gratian Lup <gratilup@microsoft.com>
2020-07-30 17:16:18 -0700

    Implement shared session download, UI, fix some bugs

--- src/IRExplorer/Sharing/SessionSharingPanel.xaml.cs
+++ src/IRExplorer/Sharing/SessionSharingPanel.xaml.cs
@@ -1,40 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using IRExplorer.OptionsPanels;
 
-namespace IRExplorer.Sharing {
-    public partial class SessionSharingPanel : OptionsPanelBase {
-        public const double DefaultHeight = 90;
-        public const double MinimumHeight = 90;
-        public const double DefaultWidth = 300;
+namespace IRExplorer {
+    public partial class SessionSharingPanel : Popup {
+        private const string DefaultConnectionString = @"<SECRET>";
+        private const string DefaultContainerName = "share";
+
+        public const double DefaultHeight = 82;
+        public const double MinimumHeight = 82;
+        public const double DefaultWidth = 400;
         public const double MinimumWidth = 100;
 
-        public string SharingLink { get; set; }
-
-        public SessionSharingPanel(string sharingLink) {
+        public SessionSharingPanel(Point position, double width, double height,
+                                   UIElement referenceElement, ISessionManager session) {
             InitializeComponent();
-            SharingLink = sharingLink;
-            DataContext = this;
+
+            Session = session;
+            var screenPosition = Utils.CoordinatesToScreen(position, referenceElement);
+            HorizontalOffset = screenPosition.X;
+            VerticalOffset = screenPosition.Y;
+            Width = width;
+            Height = height;
         }
 
-        public override void Initialize(FrameworkElement parent) {
-            base.Initialize(parent);
+        public ISessionManager Session { get; set; }
+        public string SharingLink { get; set; }
+
+        private async void ShareButton_Click(object sender, RoutedEventArgs e) {
+            var sharingLink = await UploadSessionFile();
+            DisplaySharingLink(sharingLink);
+        }
+
+        private async Task<string> UploadSessionFile() {
+            try {
+                var filePath = Path.GetTempFileName();
+
+                if (await Session.SaveSessionDocument(filePath)) {
+
+                    var sharingClient = CreateSharingClient();
+                    var result = await sharingClient.UploadSession(filePath, DefaultContainerName);
+                    return sharingClient.ToSharingLink(result);
+                }
+            }
+            catch (Exception ex) {
+
+            }
+
+            return null;
+        }
+
+        private static SessionSharing CreateSharingClient() {
+            return new SessionSharing(DefaultConnectionString);
+        }
+
+        private void DisplaySharingLink(string sharingLink) {
+            SharingLink = sharingLink;
+            DataContext = this;
             SharingLinkTextBox.SelectAll();
             SharingLinkTextBox.Focus();
             Keyboard.Focus(SharingLinkTextBox);
         }
 
+        private async void OpenButton_Click(object sender, RoutedEventArgs e) {
+            var sharingLink = SharingLinkTextBox.Text;
+            var filePath = await DownloadSessionFile(sharingLink);
+
+            if (filePath != null) {
+                await Session.OpenSessionDocument(filePath);
+                IsOpen = false;
+            }
+        }
+
+        private async Task<string> DownloadSessionFile(string sharingLink) {
+            try {
+                var sharingClient = CreateSharingClient();
+                var sharedSession = sharingClient.FromSharingLink(sharingLink);
+
+                if (sharedSession != null) {
+                    return await sharingClient.DownloadSession(sharedSession);
+                }
+            }
+            catch (Exception ex) {
+
+            }
+
+            return null;
+        }
     }
 }
