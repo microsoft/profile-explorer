6f5b47c00ec57c2a769ce86a94accadcdfb0f67a
Gratian Lup <gratilup@microsoft.com>
2020-08-14 00:02:39 -0700

    Add drop shadows under options and floating panels

--- src/IRExplorerUI/Query/QueryPanel.xaml.cs
+++ src/IRExplorerUI/Query/QueryPanel.xaml.cs
@@ -64,7 +64,7 @@ namespace IRExplorerUI.Query {
     }
 
     public partial class QueryPanel : DraggablePopup {
-        public const double DefaultHeight = 300;
+        public const double DefaultHeight = 200;
         public const double MinimumHeight = 100;
         public const double DefaultWidth = 250;
         public const double MinimumWidth = 100;
