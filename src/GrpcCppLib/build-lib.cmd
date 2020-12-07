f1a7d9cc09c8c3814fb07c6c0def3ce06ebc99bf
Gratian Lup <gratilup@microsoft.com>
2020-12-07 09:45:45 -0800

    Fix dll path in grpc build script

--- src/GrpcCppLib/build-lib.cmd
+++ src/GrpcCppLib/build-lib.cmd
@@ -8,7 +8,7 @@ rem Build x64
 msbuild GrpcCppLib.vcxproj /p:Configuration=ReleaseDLL /p:Platform=x64 /t:Rebuild /p:VCPKG_PATH=%VCPCKG_PATH%
 copy %VCPCKG_PATH%\installed\x64-windows\bin\abseil_dll.dll %cd%\GrpcCppLib\x64\ReleaseDLL
 copy %VCPCKG_PATH%\installed\x64-windows\bin\cares.dll %cd%\GrpcCppLib\x64\ReleaseDLL
-copy %VCPCKG_PATH%\installed\x64-windows\bin\libcrypto-1_1-x64 %cd%\GrpcCppLib\x64\ReleaseDLL
+copy %VCPCKG_PATH%\installed\x64-windows\bin\libcrypto-1_1-x64.dll %cd%\GrpcCppLib\x64\ReleaseDLL
 copy %VCPCKG_PATH%\installed\x64-windows\bin\libprotobuf.dll %cd%\GrpcCppLib\x64\ReleaseDLL
 copy %VCPCKG_PATH%\installed\x64-windows\bin\libprotobuf-lite.dll %cd%\GrpcCppLib\x64\ReleaseDLL
 copy %VCPCKG_PATH%\installed\x64-windows\bin\libprotoc.dll %cd%\GrpcCppLib\x64\ReleaseDLL
@@ -20,7 +20,7 @@ rem Build x86
 msbuild GrpcCppLib.vcxproj /p:Configuration=ReleaseDLL /p:Platform=Win32 /t:Rebuild /p:VCPKG_PATH=%VCPCKG_PATH%
 copy %VCPCKG_PATH%\installed\x86-windows\bin\abseil_dll.dll %cd%\GrpcCppLib\ReleaseDLL
 copy %VCPCKG_PATH%\installed\x86-windows\bin\cares.dll %cd%\GrpcCppLib\ReleaseDLL
-copy %VCPCKG_PATH%\installed\x86-windows\bin\libcrypto-1_1 %cd%\GrpcCppLib\ReleaseDLL
+copy %VCPCKG_PATH%\installed\x86-windows\bin\libcrypto-1_1.dll %cd%\GrpcCppLib\ReleaseDLL
 copy %VCPCKG_PATH%\installed\x86-windows\bin\libprotobuf.dll %cd%\GrpcCppLib\ReleaseDLL
 copy %VCPCKG_PATH%\installed\x86-windows\bin\libprotobuf-lite.dll %cd%\GrpcCppLib\ReleaseDLL
 copy %VCPCKG_PATH%\installed\x86-windows\bin\libprotoc.dll %cd%\GrpcCppLib\ReleaseDLL 
