8b5cba362817070db08ed754f6983069cb7e2522
Gratian Lup <gratilup@microsoft.com>
2021-01-04 11:20:46 -0800

    Improve LLVM section reader

--- src/IRExplorerCore/Compilers/LLVM/LLVMSectionReader.cs
+++ src/IRExplorerCore/Compilers/LLVM/LLVMSectionReader.cs
@@ -5,11 +5,37 @@ using System;
 
 namespace IRExplorerCore.LLVM {
     public sealed class LLVMSectionReader : SectionReaderBase, IDisposable {
-        private static readonly char[] WhitespaceChars = { ' ', '\t' };
-        private const string SectionStartLine = "*** IR Dump ";
-        private const string SectionEndLine = "***";
-        private const string SectionStartLine2 = "; IR Dump ";
-        private const string SectionEndLine2 = ";";
+        private static readonly string[] SectionStartLines = {
+            "*** IR Dump ",
+            "; IR Dump ",
+            "# *** IR Dump",
+        };
+
+        private static readonly string[] SectionEndLines = {
+            "***",
+            ";",
+            "***"
+        };
+
+        private static readonly string[] FunctionBodyStart = {
+            "define",
+            "# Machine code for function"
+        };
+
+        private static readonly string[] FunctionBodyEnd = {
+            "}",
+            "# End machine code for function"
+        };
+
+        private static readonly string[] FunctionNameStart = {
+            "define",
+            "Machine code for function"
+        };
+
+        private static readonly string[] FunctionNameEnd = {
+            "(",
+            ":"
+        };
 
         public LLVMSectionReader(string filePath, bool expectSectionHeaders = true) :
             base(filePath, expectSectionHeaders) { }
@@ -18,30 +44,50 @@ namespace IRExplorerCore.LLVM {
             base(textData, expectSectionHeaders) { }
 
         protected override bool IsSectionStart(string line) {
-            return line.StartsWith(SectionStartLine, StringComparison.Ordinal) ||
-                   line.StartsWith(SectionStartLine2, StringComparison.Ordinal);
+            foreach (var pattern in SectionStartLines) {
+                if (line.StartsWith(pattern, StringComparison.Ordinal)) {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         protected override bool IsFunctionStart(string line) {
-            return line.StartsWith("define", StringComparison.Ordinal) &&
-                   line.EndsWith("{", StringComparison.Ordinal);
+            foreach (var pattern in FunctionBodyStart) {
+                if (line.StartsWith(pattern, StringComparison.Ordinal)) {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         protected override bool IsBlockStart(string line) {
             //? TODO: Blocks seem to always start with N:
+            // For machine bb.1
             return false;
         }
 
         protected override bool IsFunctionEnd(string line) {
-            return line.StartsWith("}", StringComparison.Ordinal);
+            foreach (var pattern in FunctionBodyEnd) {
+                if (line.StartsWith(pattern, StringComparison.Ordinal)) {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         protected override string ExtractSectionName(string line) {
-            var name = ExtractSectionNameImpl(line, SectionStartLine, SectionEndLine);
-            if (name != null) return name;
+            for (int i = 0; i < SectionStartLines.Length; i++) {
+                var name = ExtractSectionNameImpl(line, SectionStartLines[i], SectionEndLines[i]);
 
-            name = ExtractSectionNameImpl(line, SectionStartLine2, SectionEndLine2);
-            if (name != null) return name;
+                if (name != null) {
+                    return name;
+                }
+            }
+
             return "";
         }
 
@@ -63,14 +109,26 @@ namespace IRExplorerCore.LLVM {
         }
 
         protected override string ExtractFunctionName(string line) {
-            // Function names start with @ and end before the ( starting the parameter list.
-            int start = line.IndexOf('@', StringComparison.Ordinal);
+            for (int i = 0; i < FunctionNameStart.Length; i++) {
+                var name = ExtractFunctionNameImpl(line, FunctionNameStart[i], FunctionNameEnd[i]);
 
-            if (start == -1) {
-                return "";
+                if (name != null) {
+                    return name;
+                }
             }
 
-            int end = line.IndexOf('(', start + 1);
+            return "";
+        }
+
+        private string ExtractFunctionNameImpl(string line, string nameStartMarker, string nameEndMarker) {
+            // Function names start with @ and end before the ( starting the parameter list.
+            int start = line.IndexOf(nameStartMarker, StringComparison.Ordinal);
+
+            if (start == -1) {
+                return null;
+            }
+
+            int end = line.IndexOf(nameEndMarker, start + 1, StringComparison.Ordinal);
             int length = end - start - 1;
 
             if (length > 0) {
@@ -83,7 +141,7 @@ namespace IRExplorerCore.LLVM {
                 return line.Substring(start + 1, length);
             }
 
-            return "";
+            return null;
         }
 
         protected override string PreprocessLine(string line) {
