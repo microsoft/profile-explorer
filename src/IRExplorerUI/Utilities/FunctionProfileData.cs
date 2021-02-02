75057651037a0cede24505da4c12639a2ea132d4
Gratian Lup <gratilup@microsoft.com>
2021-02-02 01:07:06 -0800

    Read and mark instruction-level profile, metadata reading and filtering rework

---
+++ src/IRExplorerUI/Utilities/FunctionProfileData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IRExplorerUI.Utilities {
+    public class FunctionProfileData {
+        public string SourceFilePath { get; set; }
+        public TimeSpan Weight { get; set; }
+        public Dictionary<int, TimeSpan> SourceLineWeight { get; set; }
+        public Dictionary<long, TimeSpan> InstructionWeight { get; set; }
+
+        public FunctionProfileData(string filePath) {
+            SourceFilePath = filePath;
+            Weight = TimeSpan.Zero;
+            SourceLineWeight = new Dictionary<int, TimeSpan>();
+            InstructionWeight = new Dictionary<long, TimeSpan>();
+        }
+
+        public void AddLineSample(int sourceLine, TimeSpan weight) {
+            if (SourceLineWeight.TryGetValue(sourceLine, out var currentWeight)) {
+                SourceLineWeight[sourceLine] = currentWeight + weight;
+            }
+            else {
+                SourceLineWeight[sourceLine] = weight;
+            }
+        }
+
+        public void AddInstructionSample(long instrOffset, TimeSpan weight) {
+            if (InstructionWeight.TryGetValue(instrOffset, out var currentWeight)) {
+                InstructionWeight[instrOffset] = currentWeight + weight;
+            }
+            else {
+                InstructionWeight[instrOffset] = weight;
+            }
+        }
+
+        public double ScaleWeight(TimeSpan weight) {
+            return (double)weight.Ticks / (double)Weight.Ticks;
+        }
+    }
+}
