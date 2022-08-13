3150cbe298839e537ac34fd2a0888d882b25da78
Gratian Lup <lgratian@gmail.com>
2022-08-13 11:51:34 -0700

    Some cleanup

--- src/IRExplorerUI/Profile/IProfileDataProvider.cs
+++ src/IRExplorerUI/Profile/IProfileDataProvider.cs
@@ -7,332 +7,48 @@ using IRExplorerUI.Compilers;
 using IRExplorerUI.Profile;
 using ProtoBuf;
 
-namespace IRExplorerUI.Profile {
-    public enum ProfileLoadStage {
-        TraceLoading,
-        SymbolLoading,
-        TraceProcessing,
-        PerfCounterProcessing
-    }
+namespace IRExplorerUI.Profile;
 
-    public class ProfileLoadProgress {
-        public ProfileLoadProgress(ProfileLoadStage stage) {
-            Stage = stage;
-        }
+public interface IProfileDataProvider {
+    ProfileData LoadTrace(string tracePath, string imageName,
+        ProfileDataProviderOptions options,
+        SymbolFileSourceOptions symbolOptions,
+        ProfileLoadProgressHandler progressCallback,
+        CancelableTask cancelableTask = null);
 
-        public ProfileLoadStage Stage { get; set; }
-        public int Total { get; set; }
-        public int Current { get; set; }
-    }
-
-    public enum ProfileSessionKind {
-        SystemWide,
-        StartProcess,
-        AttachToProcess
-    }
-
-    [ProtoContract(SkipConstructor = true)]    
-    public class ProfileRecordingSessionOptions : SettingsBase {
-        public const int DefaultSamplingFrequency = 4000;
-        public const int MaximumSamplingFrequency = 8000;
-
-        [ProtoMember(1)]
-        public ProfileSessionKind SessionKind { get; set; }
-        [ProtoMember(2)]
-        public string ApplicationPath { get; set; }
-        [ProtoMember(3)]
-        public string ApplicationArguments { get; set; }
-        [ProtoMember(4)]
-        public string WorkingDirectory { get; set; }
-        [ProtoMember(5)]
-        public int SamplingFrequency { get; set; }
-        [ProtoMember(6)]
-        public bool ProfileDotNet { get; set; }
-        [ProtoMember(7)]
-        public bool ProfileChildProcesses { get; set; }
-        [ProtoMember(8)] 
-        public bool RecordPerformanceCounters { get; set; }
-        [ProtoMember(9)]        
-        public bool EnableEnvironmentVars { get; set; }
-        [ProtoMember(10)]
-        public List<(string Variable, string Value)> EnvironmentVariables { get; set; }
-        [ProtoMember(11)]
-        public List<PerformanceCounterConfig> PerformanceCounters { get; set; }
-
-        public List<PerformanceCounterConfig> EnabledPerformanceCounters => PerformanceCounters.FindAll(c => c.IsEnabled);
-        public bool HasWorkingDirectory => Directory.Exists(WorkingDirectory);
-
-        public ProfileRecordingSessionOptions() {
-            Reset();
-        }
-        
-        public override void Reset() {
-            ResetAndInitializeReferenceMembers();
-            SessionKind = ProfileSessionKind.SystemWide;
-            SamplingFrequency = 4000; // 4 kHz, Xperf default is 1 kHz.
-        }
-
-        [ProtoAfterDeserialization]
-        private void InitializeReferenceMembers() {
-            EnvironmentVariables ??= new List<(string Variable, string Value)>();
-            PerformanceCounters ??= new List<PerformanceCounterConfig>();
-        }
-
-        private void ResetAndInitializeReferenceMembers() {
-            EnvironmentVariables?.Clear();
-            InitializeReferenceMembers();
-        }
-
-        public bool AddPerformanceCounter(PerformanceCounterConfig config) {
-            if (!PerformanceCounters.Contains(config)) {
-                PerformanceCounters.Add(config);
-                return true;
-            }
-
-            return false;
-        }
-    }
-
-    public delegate void ProfileLoadProgressHandler(ProfileLoadProgress info);
-
-    [ProtoContract(SkipConstructor = true)]
-    public class ProfileDataProviderOptions : SettingsBase {
-        [ProtoMember(1)]
-        public bool BinarySearchPathsEnabled { get; set; }
-        [ProtoMember(2)]
-        public bool BinaryNameWhitelistEnabled { get; set; }
-        [ProtoMember(3)]
-        public bool DownloadBinaryFiles { get; set; }
-        [ProtoMember(4)]
-        public List<string> BinarySearchPaths { get; set; }
-        [ProtoMember(5)]
-        public List<string> BinaryNameWhitelist { get; set; }
-        [ProtoMember(6)]
-        public bool MarkInlinedFunctions { get; set; }
-        [ProtoMember(7)]
-        public bool IncludeKernelEvents { get; set; }
-        [ProtoMember(8)]
-        public bool IncludeAllProcesses { get; set; }
-        [ProtoMember(9)]
-        public bool IncludePerformanceCounters { get; set; }
-        [ProtoMember(10)] 
-        public ProfileRecordingSessionOptions RecordingSessionOptions { get; set; }
-        [ProtoMember(11)]
-        public List<PerformanceMetricConfig> PerformanceMetrics { get; set; }
-        //? Recent profile sessions
-
-        public bool HasBinaryNameWhitelist => BinaryNameWhitelistEnabled && BinaryNameWhitelist.Count > 0;
-        public bool HasBinarySearchPaths => BinarySearchPathsEnabled && BinarySearchPaths.Count > 0;
-
-        public ProfileDataProviderOptions() {
-            Reset();
-        }
-
-        public override void Reset() {
-            ResetAndInitializeReferenceMembers();
-            DownloadBinaryFiles = true;
-        }
-
-        public bool AddPerformanceMetric(PerformanceMetricConfig config) {
-            if (!PerformanceMetrics.Contains(config)) {
-                PerformanceMetrics.Add(config);
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool HasBinaryPath(string path) {
-            path = Utils.TryGetDirectoryName(path).ToLowerInvariant();
-            return BinarySearchPaths.Find(item => item.ToLowerInvariant() == path) != null;
-        }
-
-        public void InsertBinaryPath(string path) {
-            if (string.IsNullOrEmpty(path) || HasBinaryPath(path)) {
-                return;
-            }
-
-            path = Utils.TryGetDirectoryName(path);
-
-            if (!string.IsNullOrEmpty(path)) {
-                BinarySearchPaths.Insert(0, path);
-            }
-        }
-
-        [ProtoAfterDeserialization]
-        private void InitializeReferenceMembers() {
-            BinarySearchPaths ??= new List<string>();
-            BinaryNameWhitelist ??= new List<string>();
-            RecordingSessionOptions ??= new ProfileRecordingSessionOptions();
-            PerformanceMetrics = new List<PerformanceMetricConfig>();
-        }
-
-        private void ResetAndInitializeReferenceMembers() {
-            BinarySearchPaths?.Clear();
-            BinaryNameWhitelist?.Clear();
-            RecordingSessionOptions?.Reset();
-            InitializeReferenceMembers();
-        }
-
-        public override SettingsBase Clone() {
-            var serialized = StateSerializer.Serialize(this);
-            return StateSerializer.Deserialize<ProfileDataProviderOptions>(serialized);
-        }
-    }
-    
-    public interface IProfileDataProvider {
-        ProfileData LoadTrace(string tracePath, string imageName,
-                              ProfileDataProviderOptions options,
-                              SymbolFileSourceOptions symbolOptions,
-                              ProfileLoadProgressHandler progressCallback,
-                              CancelableTask cancelableTask = null);
-
-        Task<ProfileData> LoadTraceAsync(string tracePath, string imageName,
-                                         ProfileDataProviderOptions options,
-                                         SymbolFileSourceOptions symbolOptions,
-                                         ProfileLoadProgressHandler progressCallback,
-                                         CancelableTask cancelableTask = null);
-    }
+    Task<ProfileData> LoadTraceAsync(string tracePath, string imageName,
+        ProfileDataProviderOptions options,
+        SymbolFileSourceOptions symbolOptions,
+        ProfileLoadProgressHandler progressCallback,
+        CancelableTask cancelableTask = null);
 }
 
-
-[ProtoContract(SkipConstructor = true)]
-public class PerformanceCounterConfig : IEquatable<PerformanceCounterConfig> {
-    [ProtoMember(1)]
-    public bool IsEnabled { get; set; }
-    [ProtoMember(2)]
-    public bool IsBuiltin { get; set; }
-    [ProtoMember(3)]
-    public int Id { get; set; }
-    [ProtoMember(4)]
-    public string Name { get; set; }
-    [ProtoMember(5)]
-    public string Description { get; set; }
-    [ProtoMember(6)]
-    public int Interval { get; set; }
-    [ProtoMember(7)]
-    public int MinInterval { get; set; }
-    [ProtoMember(8)]
-    public int MaxInterval { get; set; }
-    [ProtoMember(9)]
-    public int DefaultInterval { get; set; }
-
-    public PerformanceCounterConfig(int id, string name, int defaultInterval,
-                                    int minInterval, int maxInterval, bool isBuiltin) {
-        Id = id;
-        Name = name;
-        Interval = defaultInterval;
-        DefaultInterval = defaultInterval;
-        MinInterval = minInterval;
-        MaxInterval = maxInterval;
-        IsBuiltin = isBuiltin;
-    }
-
-    public bool Equals(PerformanceCounterConfig other) {
-        if (ReferenceEquals(null, other)) {
-            return false;
-        }
-
-        if (ReferenceEquals(this, other)) {
-            return true;
-        }
-
-        return Id == other.Id && Name == other.Name;
-    }
-
-    public override bool Equals(object obj) {
-        if (ReferenceEquals(null, obj)) {
-            return false;
-        }
-
-        if (ReferenceEquals(this, obj)) {
-            return true;
-        }
-
-        if (obj.GetType() != this.GetType()) {
-            return false;
-        }
-
-        return Equals((PerformanceCounterConfig)obj);
-    }
-
-    public override int GetHashCode() {
-        return HashCode.Combine(Id, Name);
-    }
-
-    public static bool operator ==(PerformanceCounterConfig left, PerformanceCounterConfig right) {
-        return Equals(left, right);
-    }
-
-    public static bool operator !=(PerformanceCounterConfig left, PerformanceCounterConfig right) {
-        return !Equals(left, right);
-    }
+public class ProfileDataProviderReport {
+    //? list of failed module loads
+    //? list of PDB status (found, file, hash)
+    //? other errors
+    //? - similar to the UTC parser errors
 }
 
-[ProtoContract(SkipConstructor = true)]
-public class PerformanceMetricConfig : IEquatable<PerformanceMetricConfig> {
-    [ProtoMember(1)]
-    public string Name { get; set; }
-    [ProtoMember(2)]
-    public string BaseCounterName { get; set; }
-    [ProtoMember(3)]
-    public string RelativeCounterName { get; set; }
-    [ProtoMember(4)]
-    public string Description { get; set; }
-    [ProtoMember(5)]
-    public bool IsPercentage { get; set; }
-    [ProtoMember(6)]
-    public bool IsEnabled { get; set; }
+public enum ProfileLoadStage {
+    TraceLoading,
+    SymbolLoading,
+    TraceProcessing,
+    PerfCounterProcessing
+}
 
-    public PerformanceMetricConfig(string name, string baseCounterName, string relativeCounterName, 
-                                   bool isPercentage, string description) {
-        Name = name;
-        BaseCounterName = baseCounterName;
-        RelativeCounterName = relativeCounterName;
-        Description = description;
-        IsPercentage = isPercentage;
-        IsEnabled = true;
+public class ProfileLoadProgress {
+    public ProfileLoadProgress(ProfileLoadStage stage) {
+        Stage = stage;
     }
 
-    public bool Equals(PerformanceMetricConfig other) {
-        if (ReferenceEquals(null, other)) {
-            return false;
-        }
+    public ProfileLoadStage Stage { get; set; }
+    public int Total { get; set; }
+    public int Current { get; set; }
+}
 
-        if (ReferenceEquals(this, other)) {
-            return true;
-        }
-
-        return Name == other.Name && 
-               BaseCounterName == other.BaseCounterName && 
-               RelativeCounterName == other.RelativeCounterName;
-    }
-
-    public override bool Equals(object obj) {
-        if (ReferenceEquals(null, obj)) {
-            return false;
-        }
-
-        if (ReferenceEquals(this, obj)) {
-            return true;
-        }
-
-        if (obj.GetType() != this.GetType()) {
-            return false;
-        }
-
-        return Equals((PerformanceMetricConfig)obj);
-    }
-
-    public override int GetHashCode() {
-        return HashCode.Combine(Name, BaseCounterName, RelativeCounterName, IsPercentage);
-    }
-
-    public static bool operator ==(PerformanceMetricConfig left, PerformanceMetricConfig right) {
-        return Equals(left, right);
-    }
-
-    public static bool operator !=(PerformanceMetricConfig left, PerformanceMetricConfig right) {
-        return !Equals(left, right);
-    }
+public enum ProfileSessionKind {
+    SystemWide,
+    StartProcess,
+    AttachToProcess
 }
---
+++ src/IRExplorerUI/Profile/PerformanceCounters.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using ProtoBuf;
+
+namespace IRExplorerUI.Profile;
+
+[ProtoContract(SkipConstructor = true)]
+public class PerformanceCounterConfig : IEquatable<PerformanceCounterConfig> {
+    [ProtoMember(1)]
+    public bool IsEnabled { get; set; }
+    [ProtoMember(2)]
+    public bool IsBuiltin { get; set; }
+    [ProtoMember(3)]
+    public int Id { get; set; }
+    [ProtoMember(4)]
+    public string Name { get; set; }
+    [ProtoMember(5)]
+    public string Description { get; set; }
+    [ProtoMember(6)]
+    public int Interval { get; set; }
+    [ProtoMember(7)]
+    public int MinInterval { get; set; }
+    [ProtoMember(8)]
+    public int MaxInterval { get; set; }
+    [ProtoMember(9)]
+    public int DefaultInterval { get; set; }
+
+    public PerformanceCounterConfig(int id, string name, int defaultInterval,
+                                    int minInterval, int maxInterval, bool isBuiltin) {
+        Id = id;
+        Name = name;
+        Interval = defaultInterval;
+        DefaultInterval = defaultInterval;
+        MinInterval = minInterval;
+        MaxInterval = maxInterval;
+        IsBuiltin = isBuiltin;
+    }
+
+    public bool Equals(PerformanceCounterConfig other) {
+        if (ReferenceEquals(null, other)) {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other)) {
+            return true;
+        }
+
+        return Id == other.Id && Name == other.Name;
+    }
+
+    public override bool Equals(object obj) {
+        if (ReferenceEquals(null, obj)) {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj)) {
+            return true;
+        }
+
+        if (obj.GetType() != this.GetType()) {
+            return false;
+        }
+
+        return Equals((PerformanceCounterConfig)obj);
+    }
+
+    public override int GetHashCode() {
+        return HashCode.Combine(Id, Name);
+    }
+
+    public static bool operator ==(PerformanceCounterConfig left, PerformanceCounterConfig right) {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(PerformanceCounterConfig left, PerformanceCounterConfig right) {
+        return !Equals(left, right);
+    }
+}
+
+
+
+[ProtoContract(SkipConstructor = true)]
+public class PerformanceMetricConfig : IEquatable<PerformanceMetricConfig> {
+    [ProtoMember(1)]
+    public string Name { get; set; }
+    [ProtoMember(2)]
+    public string BaseCounterName { get; set; }
+    [ProtoMember(3)]
+    public string RelativeCounterName { get; set; }
+    [ProtoMember(4)]
+    public string Description { get; set; }
+    [ProtoMember(5)]
+    public bool IsPercentage { get; set; }
+    [ProtoMember(6)]
+    public bool IsEnabled { get; set; }
+
+    public PerformanceMetricConfig(string name, string baseCounterName, string relativeCounterName,
+                                   bool isPercentage, string description) {
+        Name = name;
+        BaseCounterName = baseCounterName;
+        RelativeCounterName = relativeCounterName;
+        Description = description;
+        IsPercentage = isPercentage;
+        IsEnabled = true;
+    }
+
+    public bool Equals(PerformanceMetricConfig other) {
+        if (ReferenceEquals(null, other)) {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other)) {
+            return true;
+        }
+
+        return Name == other.Name &&
+               BaseCounterName == other.BaseCounterName &&
+               RelativeCounterName == other.RelativeCounterName;
+    }
+
+    public override bool Equals(object obj) {
+        if (ReferenceEquals(null, obj)) {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj)) {
+            return true;
+        }
+
+        if (obj.GetType() != this.GetType()) {
+            return false;
+        }
+
+        return Equals((PerformanceMetricConfig)obj);
+    }
+
+    public override int GetHashCode() {
+        return HashCode.Combine(Name, BaseCounterName, RelativeCounterName, IsPercentage);
+    }
+
+    public static bool operator ==(PerformanceMetricConfig left, PerformanceMetricConfig right) {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(PerformanceMetricConfig left, PerformanceMetricConfig right) {
+        return !Equals(left, right);
+    }
+}
