3b6536146f5a26359c9f308e848b9e417a95a42e
Benjamin Galindo-Navarro <benjaming@microsoft.com>
2023-10-09 14:37:29 -0700

    Enable IR Explorer to be opened from command line with a trace, symbol path, pid, and process image name.

--- src/IRExplorerUI/Profile/ETW/ETWProfileDataProvider.cs
+++ src/IRExplorerUI/Profile/ETW/ETWProfileDataProvider.cs
@@ -86,6 +86,11 @@ public sealed partial class ETWProfileDataProvider : IProfileDataProvider, IDisp
             return eventProcessor.ProcessEvents(progressCallback, cancelableTask);
         });
 
+        if (profile.FindProcess(processIds[0]) == null) {
+            Trace.WriteLine($"Failed to find main process id {processIds[0]} in trace.");
+            return null;
+        }
+
         var result = await LoadTraceAsync(profile, processIds, options, symbolOptions,
                                           report, progressCallback, cancelableTask);
         profile.Dispose();
