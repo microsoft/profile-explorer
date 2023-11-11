8a673c8e5c1a2514e493e1ce3981b395e817a6fb
Gratian Lup <lgratian@gmail.com>
2023-11-11 01:35:01 -0800

    Add and update license headers

--- src/IRExplorerUI/Profile/FlameGraph/QuadTree.cs
+++ src/IRExplorerUI/Profile/FlameGraph/QuadTree.cs
@@ -1,7 +1,6 @@
-﻿//----------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-//----------------------------------------------------------------
-// https://referencesource.microsoft.com/#System.Activities.Presentation/System.Activities.Presentation/System/Activities/Presentation/View/QuadTree.cs
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 namespace IRExplorerUI.Profile;
 
--- src/IRExplorerUI/Scripting/ScriptManager.cs
+++ src/IRExplorerUI/Scripting/ScriptManager.cs
@@ -1,4 +1,8 @@
-﻿namespace IRExplorerUI.Scripting {
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace IRExplorerUI.Scripting {
     public class ScriptManager {
         // default scripts, load user scripts
         // import/export
