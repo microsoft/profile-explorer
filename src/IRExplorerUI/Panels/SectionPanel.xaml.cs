42a21e12a687d606217c5f50783dcdb16723927a
Gratian Lup <lgratian@gmail.com>
2023-11-14 20:56:23 -0800

    Remove Trace

--- src/IRExplorerUI/Panels/SectionPanel.xaml.cs
+++ src/IRExplorerUI/Panels/SectionPanel.xaml.cs
@@ -1750,7 +1750,6 @@ public partial class SectionPanel : ToolPanelControl, INotifyPropertyChanged {
   private void CreateFunctionExtensions(IRTextSummary summary, List<IRTextFunctionEx> functionsEx) {
     foreach (var func in summary.Functions) {
       if (IsHiddenFunction(func)) {
-        Trace.WriteLine($"Ignore {func.Name}");
         continue;
       }
 
@@ -1830,7 +1829,6 @@ public partial class SectionPanel : ToolPanelControl, INotifyPropertyChanged {
   private void SetupSectionExtensions(IRTextSummary summary) {
     foreach (var func in summary.Functions) {
       if (IsHiddenFunction(func)) {
-          Trace.WriteLine($"Ignore section {func.Name}");
         continue;
       }
 
