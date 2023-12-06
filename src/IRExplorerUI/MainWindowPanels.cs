05a5d38d5e389e9ed79842a6948c0c1aa40542d2
Gratian Lup <gratilup@microsoft.com>
2023-12-06 11:59:11 -0800

    Set active workspace on initial run, sync combobox

--- src/IRExplorerUI/MainWindowPanels.cs
+++ src/IRExplorerUI/MainWindowPanels.cs
@@ -992,13 +992,14 @@ public partial class MainWindow : Window, ISession {
   }
 
   private bool RestoreDockLayout() {
-    if (!initialDockLayoutRestored_ || // Initial load and registration of active panel config.
-        App.Settings.WorkspaceOptions.RestoreDefaultActiveWorkspace()) {
+    if (App.Settings.WorkspaceOptions.RestoreDefaultActiveWorkspace() ||
+        !initialDockLayoutRestored_) { // Initial load and registration of active panel config.
       string dockLayoutFile = null;
       var activeWs = App.Settings.WorkspaceOptions.ActiveWorkspace;
 
       if (activeWs != null) {
         dockLayoutFile = activeWs.FilePath;
+        WorkspaceCombobox.SelectedIndex = activeWs.Order;
       }
       else {
         dockLayoutFile = App.GetDefaultDockLayoutFilePath();
