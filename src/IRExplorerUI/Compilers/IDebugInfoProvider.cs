26651c45d0fc48c34ab31123d6d73aec72cc20d2
Gratian Lup <gratilup@microsoft.com>
2024-01-22 18:20:09 -0800

    Show _NT_SYMBOL_PATH as tooltip

--- src/IRExplorerUI/Compilers/IDebugInfoProvider.cs
+++ src/IRExplorerUI/Compilers/IDebugInfoProvider.cs
@@ -87,30 +87,3 @@ public class SymbolFileDescriptor : IEquatable<SymbolFileDescriptor> {
            Age == other.Age;
   }
 }
-
-class NetAPI32 {
-  [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-  public struct DSREG_JOIN_INFO {
-    public int joinType;
-    public IntPtr pJoinCertificate;
-    [MarshalAs(UnmanagedType.LPWStr)] public string DeviceId;
-    [MarshalAs(UnmanagedType.LPWStr)] public string IdpDomain;
-    [MarshalAs(UnmanagedType.LPWStr)] public string TenantId;
-    [MarshalAs(UnmanagedType.LPWStr)] public string JoinUserEmail;
-    [MarshalAs(UnmanagedType.LPWStr)] public string TenantDisplayName;
-    [MarshalAs(UnmanagedType.LPWStr)] public string MdmEnrollmentUrl;
-    [MarshalAs(UnmanagedType.LPWStr)] public string MdmTermsOfUseUrl;
-    [MarshalAs(UnmanagedType.LPWStr)] public string MdmComplianceUrl;
-    [MarshalAs(UnmanagedType.LPWStr)] public string UserSettingSyncUrl;
-    public IntPtr pUserInfo;
-  }
-
-  [DllImport("netapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-  public static extern void NetFreeAadJoinInformation(
-    IntPtr pJoinInfo);
-
-  [DllImport("netapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-  public static extern int NetGetAadJoinInformation(
-    string pcszTenantId,
-    out IntPtr ppJoinInfo);
-}
